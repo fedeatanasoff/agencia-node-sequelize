14a9daeb3a06b0c2ce1dade1f296449754b6dbe9
fedeatanasoff <fede.atanasoff@gmail.com>
2019-04-14 03:42:25 -0300

    agregando primeras vistas.pug instalado

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes/routes.js");
+const pug = require("pug");
+const path = require("path");
 
-app.use("/", (req, res) => {
-  res.send("hello friend");
-});
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "./views"));
+app.use("/", routes());
 
 app.listen(2100, () => console.log(`Servidor corriendo en el puerto 2100`));
